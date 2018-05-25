019f50a5290e380cf4f0b6d8bdbf6f0c9235cbad
vinh_hoa.la <vinh_hoa.la@bitbucket.com>
2018-05-26 01:54:58 +0200

    ++

--- www/routes/index.js
+++ www/routes/index.js
@@ -37,18 +37,63 @@ router.post('/search', function(req, res, next){
 				  }
 				  else {
 					  //user.id, user.name, user.tags
-					  db.collection("places").findOne({tag: req.body.tags}, function(erro, place){
+					  db.collection("places").find({tag: req.body.tags}, function(erro, place){
 						  if (erro || (place === null)){
 							  res.render('error', { message: 'Error', error:{status:'Find place failed', stack: ''} });
 						  }
 						  else {
-							  //place.name, place.description, place.price, place.tag, place.photo 
+							  //place[i].name, place[i].description, place[i].price, place[i].tag, place[i].photo 
 						  }
 					  });
 				  }
 			  });
 	}
 });
+
+function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+	  var R = 6371; // Radius of the earth in km
+	  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+	  var dLon = deg2rad(lon2-lon1); 
+	  var a = 
+	    Math.sin(dLat/2) * Math.sin(dLat/2) +
+	    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+	    Math.sin(dLon/2) * Math.sin(dLon/2)
+	    ; 
+	  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+	  var d = R * c; // Distance in km
+	  return d;
+	}
+
+function deg2rad(deg) {
+	  return deg * (Math.PI/180)
+	}
+
+router.post('/nearby', function(req, res, next){
+	if (req.body.tags && 
+		req.body.user_id && 
+		req.body.location){
+					  db.collection("places").find({tag: req.body.tags}, function(erro, place){
+						  if (erro || (place === null)){
+							  res.render('error', { message: 'Error', error:{status:'Find place failed', stack: ''} });
+						  }
+						  else {
+							  //place[i].name, place[i].description, place[i].price, place[i].tag, place[i].photo 
+							  //getDistanceFromLatLonInKm < 1km : Display 
+						  }
+					  });
+	}
+});
+
+var allTags = [
+    "monument",
+    "sight",
+    "museum",
+    "concert",
+    "shopping",
+    "food",
+    "nightlift",
+    "casino"
+];
 
 module.exports = router;
 
